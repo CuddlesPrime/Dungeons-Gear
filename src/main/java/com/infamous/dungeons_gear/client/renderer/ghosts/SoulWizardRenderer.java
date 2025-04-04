322ba7391136e1fe4c77ff9ad06fe324ae163b41
Cody Bolling <cjbolling6798@gmail.com>
2025-04-04 18:01:01 +0000

    update soul wizard renderer

--- src/main/java/com/infamous/dungeons_gear/client/renderer/ghosts/SoulWizardRenderer.java
+++ src/main/java/com/infamous/dungeons_gear/client/renderer/ghosts/SoulWizardRenderer.java
@@ -1,22 +1,22 @@
 package com.infamous.dungeons_gear.client.renderer.ghosts;
 
+import javax.annotation.Nullable;
+
 import com.infamous.dungeons_gear.DungeonsGear;
 import com.infamous.dungeons_gear.client.models.ghosts.SoulWizardModel;
 import com.infamous.dungeons_gear.client.renderer.layers.PulsatingGlowLayer;
 import com.infamous.dungeons_gear.entities.SoulWizardEntity;
 import com.mojang.blaze3d.vertex.PoseStack;
-import com.mojang.blaze3d.vertex.VertexConsumer;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.resources.ResourceLocation;
-import software.bernie.geckolib3.renderers.geo.GeoEntityRenderer;
+import software.bernie.geckolib.renderer.GeoEntityRenderer;
 
 public class SoulWizardRenderer extends GeoEntityRenderer<SoulWizardEntity> {
-
     public SoulWizardRenderer(EntityRendererProvider.Context renderManager) {
         super(renderManager, new SoulWizardModel());
-        this.addLayer(new PulsatingGlowLayer<>(this, ResourceLocation.fromNamespaceAndPath(DungeonsGear.MODID, "textures/entity/ghosts/soul_wizard_glow.png"), 0.2F, 1.0F, 0.25F));
+        this.addRenderLayer(new PulsatingGlowLayer<SoulWizardEntity>(this, ResourceLocation.fromNamespaceAndPath(DungeonsGear.MODID, "textures/entity/ghosts/soul_wizard_glow.png"), 0.2F, 1.0F, 0.25F));
     }
 
     @Override
@@ -30,13 +30,15 @@ public class SoulWizardRenderer extends GeoEntityRenderer<SoulWizardEntity> {
         }
         matrixStackIn.scale(scaleFactor, scaleFactor, scaleFactor);
         super.applyRotations(entityLiving, matrixStackIn, ageInTicks, rotationYaw, partialTicks);
-
     }
 
     @Override
-    public RenderType getRenderType(SoulWizardEntity animatable, float partialTicks, PoseStack stack,
-                                    MultiBufferSource renderTypeBuffer, VertexConsumer vertexBuilder, int packedLightIn,
-                                    ResourceLocation textureLocation) {
-        return RenderType.entityTranslucent(getTextureLocation(animatable));
+    public RenderType getRenderType(SoulWizardEntity animatable, ResourceLocation texture, @Nullable MultiBufferSource bufferSource, float partialTick) {
+        return RenderType.entityTranslucent(texture);
+    }
+
+    @Override
+    public ResourceLocation getTextureLocation(@Nullable SoulWizardEntity p_114482_) {
+        return this.model.getTextureResource(p_114482_, this);
     }
 }
--- src/main/java/com/infamous/dungeons_gear/client/renderer/layers/PulsatingGlowLayer.java
+++ src/main/java/com/infamous/dungeons_gear/client/renderer/layers/PulsatingGlowLayer.java
@@ -1,6 +1,8 @@
 package com.infamous.dungeons_gear.client.renderer.layers;
 
 import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
+
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.resources.ResourceLocation;
@@ -8,22 +10,21 @@ import net.minecraft.util.Mth;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
-import software.bernie.geckolib3.core.IAnimatable;
-import software.bernie.geckolib3.model.provider.GeoModelProvider;
-import software.bernie.geckolib3.renderers.geo.GeoLayerRenderer;
-import software.bernie.geckolib3.renderers.geo.IGeoRenderer;
+import software.bernie.geckolib.animatable.GeoEntity;
+import software.bernie.geckolib.cache.object.BakedGeoModel;
+import software.bernie.geckolib.renderer.GeoRenderer;
+import software.bernie.geckolib.renderer.layer.GeoRenderLayer;
 
 @OnlyIn(Dist.CLIENT)
-public class PulsatingGlowLayer<T extends LivingEntity & IAnimatable> extends GeoLayerRenderer<T> {
+public class PulsatingGlowLayer<T extends LivingEntity & GeoEntity> extends GeoRenderLayer<T> {
 
-    public ResourceLocation textureLocation;
+    private final ResourceLocation textureLocation;
+    private final float pulseSpeed;
+    private final float pulseAmount;
+    private final float minimumPulseAmount;
 
-    public float pulseSpeed;
-    public float pulseAmount;
-    public float minimumPulseAmount;
-
-    public PulsatingGlowLayer(IGeoRenderer<T> endermanReplacementRenderer, ResourceLocation textureLocation, float pulseSpeed, float pulseAmount, float minimumPulseAmount) {
-        super(endermanReplacementRenderer);
+    public PulsatingGlowLayer(GeoRenderer<T> renderer, ResourceLocation textureLocation, float pulseSpeed, float pulseAmount, float minimumPulseAmount) {
+        super(renderer);
         this.textureLocation = textureLocation;
         this.pulseSpeed = pulseSpeed;
         this.pulseAmount = pulseAmount;
@@ -31,22 +32,29 @@ public class PulsatingGlowLayer<T extends LivingEntity & IAnimatable> extends Ge
     }
 
     @Override
-    public void render(PoseStack matrixStackIn, MultiBufferSource bufferIn, int packedLightIn,
-                       T entitylivingbaseIn, float limbSwing, float limbSwingAmount, float partialTicks,
-                       float ageInTicks, float netHeadYaw, float headPitch) {
-
-        GeoModelProvider<T> geomodel = this.getEntityModel();
-
-        // original speed: 0.045F
-        // original amount: 0.25F
-
-        float glow = Math.max(minimumPulseAmount, Mth.cos(ageInTicks * pulseSpeed) * pulseAmount);
-        renderModel(geomodel, textureLocation, matrixStackIn, bufferIn, packedLightIn, entitylivingbaseIn, 1.0F, glow, glow, glow);
-    }
+    protected ResourceLocation getTextureResource(T animatable) {
+		return textureLocation;
+	}
 
     @Override
-    public RenderType getRenderType(ResourceLocation textureLocation) {
-        return RenderType.eyes(textureLocation);
-    }
+    public void render(PoseStack poseStack, T animatable, BakedGeoModel bakedModel, RenderType renderType,
+						MultiBufferSource bufferSource, VertexConsumer buffer, float partialTick,
+						int packedLight, int packedOverlay) {
+        float ageInTicks = animatable.tickCount + partialTick;
+        float glow = Math.max(minimumPulseAmount, Mth.cos(ageInTicks * pulseSpeed) * pulseAmount);
+        VertexConsumer vertexConsumer = bufferSource.getBuffer(renderType);
 
+        this.getRenderer().reRender(
+            bakedModel,
+            poseStack,
+            bufferSource,
+            animatable,
+            renderType,
+            vertexConsumer,
+            partialTick,
+            packedLight,
+            packedOverlay,
+            1.0F, glow, glow, glow
+        );
+    }
 }
