498e3b9f2ee72ee4f50f213810347948d7a784f3
Cody Bolling <cjbolling6798@gmail.com>
2025-04-05 20:19:02 +0000

    update armor constructors to match new amror gear params in dungeons library

--- src/main/java/com/infamous/dungeons_gear/items/armor/FreezingResistanceArmorGear.java
+++ src/main/java/com/infamous/dungeons_gear/items/armor/FreezingResistanceArmorGear.java
@@ -2,12 +2,12 @@ package com.infamous.dungeons_gear.items.armor;
 
 import com.infamous.dungeons_libraries.items.gearconfig.ArmorGear;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.item.ArmorItem;
 
 public class FreezingResistanceArmorGear extends ArmorGear {
 
-    public FreezingResistanceArmorGear(EquipmentSlot slotType, Properties properties, ResourceLocation armorSet, ResourceLocation modelLocation, ResourceLocation textureLocation, ResourceLocation animationFileLocation) {
-        super(slotType, properties, armorSet, modelLocation, textureLocation, animationFileLocation);
+    public FreezingResistanceArmorGear(ArmorItem.Type armorType, Properties properties, ResourceLocation armorSet, ResourceLocation modelLocation, ResourceLocation textureLocation, ResourceLocation animationFileLocation) {
+        super(armorType, properties, armorSet, modelLocation, textureLocation, animationFileLocation);
     }
 
     public double getFreezingResistance() {
--- src/main/java/com/infamous/dungeons_gear/items/armor/PetBatArmorGear.java
+++ src/main/java/com/infamous/dungeons_gear/items/armor/PetBatArmorGear.java
@@ -2,12 +2,12 @@ package com.infamous.dungeons_gear.items.armor;
 
 import com.infamous.dungeons_libraries.items.gearconfig.ArmorGear;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.item.ArmorItem;
 
 public class PetBatArmorGear extends ArmorGear {
 
-    public PetBatArmorGear(EquipmentSlot slotType, Properties properties, ResourceLocation armorSet, ResourceLocation modelLocation, ResourceLocation textureLocation, ResourceLocation animationFileLocation) {
-        super(slotType, properties, armorSet, modelLocation, textureLocation, animationFileLocation);
+    public PetBatArmorGear(ArmorItem.Type armorType, Properties properties, ResourceLocation armorSet, ResourceLocation modelLocation, ResourceLocation textureLocation, ResourceLocation animationFileLocation) {
+        super(armorType, properties, armorSet, modelLocation, textureLocation, animationFileLocation);
     }
 
     public boolean doGivesYouAPetBat() {
