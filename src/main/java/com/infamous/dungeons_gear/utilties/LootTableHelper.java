4bc57e90820184ad46444146c1ad00d0f035cd19
Cody Bolling <cjbolling6798@gmail.com>
2025-04-06 00:18:49 +0000

    update loot table helper

--- src/main/java/com/infamous/dungeons_gear/utilties/LootTableHelper.java
+++ src/main/java/com/infamous/dungeons_gear/utilties/LootTableHelper.java
@@ -1,6 +1,5 @@
 package com.infamous.dungeons_gear.utilties;
 
-import com.google.common.collect.Sets;
 import com.infamous.dungeons_gear.mixin.LootContextAccessor;
 import net.minecraft.core.BlockPos;
 import net.minecraft.resources.ResourceLocation;
@@ -8,66 +7,58 @@ import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.storage.loot.LootContext;
+import net.minecraft.world.level.storage.loot.LootParams;
 import net.minecraft.world.level.storage.loot.LootTable;
-import net.minecraft.world.level.storage.loot.parameters.LootContextParam;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParams;
 import net.minecraft.world.phys.Vec3;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
 public class LootTableHelper {
     public static ItemStack generateItemStack(ServerLevel world, BlockPos pos, ResourceLocation lootTable, RandomSource random) {
-        LootContext context = new LootContext.Builder(world)
-                .withRandom(random)
-                .withParameter(LootContextParams.ORIGIN, Vec3.atCenterOf(pos)) // positional context
-                .create(LootContextParamSets.CHEST);    // chest set requires positional context, has no other mandatory parameters
+        LootParams lootParams = new LootParams.Builder(world)
+            .withParameter(LootContextParams.ORIGIN, Vec3.atCenterOf(pos))
+            .create(LootContextParamSets.CHEST);
+        LootTable table = world.getServer().getLootData().getLootTable(lootTable);
+        List<ItemStack> stacks = table.getRandomItems(lootParams);
 
-        LootTable table = world.getServer()
-                .getLootTables()
-                .get(lootTable);
-        List<ItemStack> stacks = table.getRandomItems(context);
         return !stacks.isEmpty()
                 ? stacks.get(0)
                 : ItemStack.EMPTY;
     }
 
-    public static List<ItemStack> generateItemStacks(ServerLevel world, Vec3 pos, ResourceLocation lootTable, RandomSource random) {
-        LootContext context = new LootContext.Builder(world)
-                .withRandom(random)
-                .withParameter(LootContextParams.ORIGIN, pos) // positional context
-                .create(LootContextParamSets.CHEST);    // chest set requires positional context, has no other mandatory parameters
+    public static List<ItemStack> generateItemStacks(ServerLevel world, BlockPos pos, ResourceLocation lootTable, RandomSource random) {
+        LootParams lootParams = new LootParams.Builder(world)
+            .withParameter(LootContextParams.ORIGIN, Vec3.atCenterOf(pos))
+            .create(LootContextParamSets.CHEST);   // chest set requires positional context, has no other mandatory parameters
+        LootTable table = world.getServer().getLootData().getLootTable(lootTable);
 
-        LootTable table = world.getServer()
-                .getLootTables()
-                .get(lootTable);
-        return table.getRandomItems(context);
+        return table.getRandomItems(lootParams);
     }
 
     public static List<ItemStack> generateItemStacks(ServerLevel world, LootContext originContext, ResourceLocation lootTable) {
         if (!isCompleteParameterSet(originContext)) return new ArrayList<>();
         LootContext newContext = copyLootContextWithNewQueryID(originContext, lootTable);
-        return originContext.getLootTable(lootTable).getRandomItems(newContext);
+        LootTable table = world.getServer().getLootData().getLootTable(lootTable);
+
+        return table.getRandomItems(newContext.params);
     }
 
     private static boolean isCompleteParameterSet(LootContext originContext) {
-        Map<LootContextParam<?>, Object> originParams = originContext.params;
-        Set<LootContextParam<?>> set1 = Sets.difference(LootContextParamSets.CHEST.getRequired(), originParams.keySet());
-        return set1.isEmpty();
+        return originContext.params.hasParam(LootContextParams.ORIGIN);
     }
 
     public static boolean lootTableExists(ServerLevel world, ResourceLocation lootTable) {
         return !world.getServer()
-                .getLootTables()
-                .get(lootTable)
-                .equals(LootTable.EMPTY);
+            .getLootData()
+            .getLootTable(lootTable)
+            .equals(LootTable.EMPTY);
     }
 
     protected static LootContext copyLootContextWithNewQueryID(LootContext oldLootContext, ResourceLocation newQueryID) {
-        LootContext newContext = new LootContext.Builder(oldLootContext).create(LootContextParamSets.CHEST);
+        LootContext newContext = new LootContext.Builder(oldLootContext).create(null);
         ((LootContextAccessor) newContext).dungeonsgear_setQueriedLootTableId(newQueryID);
         return newContext;
     }
