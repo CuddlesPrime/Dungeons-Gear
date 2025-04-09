5a929a703854158de43177b18b02ec55911be51c
Cody Bolling <cjbolling6798@gmail.com>
2025-04-09 06:00:52 +0000

    it's running but crashing on load

--- src/main/java/com/infamous/dungeons_gear/mixin/PlayerEntityMixin.java
+++ src/main/java/com/infamous/dungeons_gear/mixin/PlayerEntityMixin.java
@@ -2,14 +2,18 @@ package com.infamous.dungeons_gear.mixin;
 
 import com.infamous.dungeons_gear.capabilities.combo.Combo;
 import com.infamous.dungeons_gear.capabilities.combo.ComboHelper;
+import com.infamous.dungeons_gear.registry.AttributeInit;
+
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Player.class)
 public abstract class PlayerEntityMixin extends LivingEntity {
@@ -17,13 +21,13 @@ public abstract class PlayerEntityMixin extends LivingEntity {
         super(entityType_1, world_1);
     }
 
-    // @Inject(
-    //         method = "Lnet/minecraft/world/entity/player/Player;createAttributes()Lnet/minecraft/world/entity/ai/attributes/AttributeSupplier$Builder;",
-    //         at = @At("RETURN")
-    // )
-    // private static void initAttributes(CallbackInfoReturnable<AttributeSupplier.Builder> ci) {
-    //     ci.getReturnValue().add(AttributeInit.ATTACK_REACH.get());
-    // }
+    @Inject(
+            method = "Lnet/minecraft/world/entity/player/Player;createAttributes()Lnet/minecraft/world/entity/ai/attributes/AttributeSupplier$Builder;",
+            at = @At("RETURN")
+    )
+    private static void initAttributes(CallbackInfoReturnable<AttributeSupplier.Builder> ci) {
+        ci.getReturnValue().add(AttributeInit.ATTACK_REACH.get());
+    }
 
 //    @ModifyConstant(
 //            method = "attackTargetEntityWithCurrentItem",
